c4998ceaf55c34265bfb721f681a2a404f278cc7
Martin K <thedeadgamer101@gmail.com>
2024-04-03 23:35:23 +0300

    GuildMemberAdd custom start role

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const config = require('../config.json')
+const { token } = require('../config.json');
 
 const client = new Client({
   intents: [
@@ -7,6 +7,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
   ]
 });
 
@@ -14,12 +15,30 @@ client.on('ready', (c) => {
   console.log(` ${c.user.tag} is online`);
 });
 
-client.login(config.token)
+client.login(token)
 .then((fulfill) => console.log("Connected"))
 .catch((err) => console.log(err));
 
-client.on("messageCreate", async (message) => {
-  message.channel.
-  console.log(message);
-});
+client.on('guildMemberAdd', (member) => {
+  let role = member.guild.roles.cache.find(role => role.name === "Recruit");
+  if (member.roles.cache.has(role.id)) {
+    console.log("Yay, the author of the message has the role!");
+  }
+  else {
+    console.log("No he does not have that role");
+    member.roles.add(role)
+    .catch(console.error());
+  }
+})
 
+/*
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return false;
+
+  console.log(message.content);
+
+  if (message.content.includes("?") || message.content.includes("bot")) {
+    message.reply("nuh uh");
+  }
+});
+*/
